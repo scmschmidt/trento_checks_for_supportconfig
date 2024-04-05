c2dd0dcdae1ec5e2169404fa2b070effa4ea8a34
Sören Schmidt <sschmidt@suse.com>
2024-04-05 13:51:00 +0200

    Reading supportconfigs is more robust. Host duplicates and mixing providers are not allowed.

--- tcsc_supportfiles.py
+++ tcsc_supportfiles.py
@@ -19,6 +19,7 @@ class SupportFiles():
         self.result = {}
         self.issues = []
         
+        provider = None
         for file in [os.path.realpath(os.path.expandvars(os.path.expanduser(p))) for p in supportfiles]:
             try:
                 sc = tarfile.open(file)
@@ -33,15 +34,27 @@ class SupportFiles():
                             line = f.readline()  
                         virtualization = f.readline().decode().split(':')[1].strip()
                         if virtualization.startswith('Amazon EC2'):
-                            provider = 'aws'
+                            host_provider = 'aws'
                         elif virtualization.startswith('Microsoft Corporation'):
-                            provider = 'azure'
+                            host_provider = 'azure'
                         else:
-                            provider = 'default'
+                            host_provider = 'default'
                         
-                        self.result[hostname] = {'provider': provider,
+                        if hostname in self.result:
+                            raise SupportFileException(f'{hostname} already present. Is "{file}" used twice?')
+                        
+                        if not provider:
+                            provider = host_provider
+                        if provider != host_provider:
+                            raise SupportFileException(f'Mixing providers is not allowed. Previous supportconfigs have "{provider}", but "{file} has "{host_provider}".')
+                        
+                        self.result[hostname] = {'provider': host_provider,
                                             'supportconfig': file
                                            }    
             except Exception as err:
                 self.issues.append(err)
 
+
+
+class SupportFileException(Exception):
+    pass       
