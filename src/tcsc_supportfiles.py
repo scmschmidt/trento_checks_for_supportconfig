be8e3bfb7e21bba6697854cef15342a7503e1927
Sören Schmidt <sschmidt@suse.com>
2025-01-08 14:13:40 +0100

    started implementing rnvironment autodetection

--- src/tcsc_supportfiles.py
+++ src/tcsc_supportfiles.py
@@ -21,6 +21,8 @@ class SupportFiles():
         self.issues = []
         
         provider = None
+        type = 'host' if len(supportfiles) == 1 else 'cluster'
+        
         for file in supportfiles:            
             
             # If HOST_ROOT_FS is set, we run inside a container and all paths
@@ -84,13 +86,64 @@ class SupportFiles():
                 except:
                     host_provider = 'unknown'    
                     
-                # TODO: Add auto detection.
-                cluster_type = None
-                architecture_type = None
-                ensa_version = None
-                filesystem_type = None
-                hana_scenario = None    # NEW
-                
+                # Detect environment settings.
+                if type == 'cluster':
+                    
+                    # Detect cluster_type.
+                    # one of hana_scale_up, hana_scale_out, ascs_ers (if target_type is cluster)
+                    cluster_type = None    # TODO: Add auto detection.
+                                       
+                    # Detect filesystem_type.
+                    # one of resource_managed, simple_mount, mixed_fs_types	(if cluster_type is ascs_ers)
+                    #
+                    # If each SAP system (SID) contains the primitive type `Filesystem` we have `resource_managed`.
+                    # If each SAP system (SID) does not contain the primitive type `Filesystem` we have `simple_mount`.
+                    # Otherwise it is `mixed_fs_types`. In case of multiple SIDs, it is always `mixed_fs_types`.
+                    #
+                    # SID is not explicit part of the CIB. The presence in the group id is not guaranteed!
+                    # Therefore we have a cheap implementation:
+                    # If every group (instance) has a filesystem primitive, we have resource_managed.
+                    # If no group (instance) has a filesystem primitive, we have simple_mount.
+                    # Otherwise we have mixed_fs_types.
+                    if cluster_type == 'ascs_ers':
+                        groups = root.findall(".//group")   # finding all groups
+                        groups_with_filesystem_primitive = root.findall(".//primitive[@type='Filesystem']...")   # parent of filesystem primitive
+                        if len(groups_with_filesystem_primitive) == 0:
+                            filesystem_type = 'simple_mount'
+                        else:
+                            filesystem_type = 'resource_managed' if len(groups) == len(groups_with_filesystem_primitive) else 'mixed_fs_types'
+                    else:
+                        filesystem_type = None 
+                        
+                    # Detect hana_scenario.
+                    # one of performance_optimized, cost_optimized, unknown	(if cluster_type is hana_scale_up)
+                    if cluster_type == 'hana_scale_up':
+                        hana_scenario = None    # TODO: Add auto detection.   
+                    else:
+                        hana_scenario = 'unknown'
+                    
+                    # Detect architecture_type.
+                    # one of classic, angi	(if cluster_type is one of hana_scale_up, hana_scale_out)
+                    if cluster_type in ['hana_scale_up', 'hana_scale_out']:
+                        architecture_type = None    # TODO: Add auto detection.   
+                    else:    
+                        architecture_type = None
+                        
+                    # Detect ensa_version.
+                    # one of ensa1, ensa2, mixed_versions (if cluster_type is ascs_ers)
+                    if cluster_type == 'ascs_ers':
+                        ensa_version = None    # TODO: Add auto detection.
+                    else:
+                        ensa_version = None    
+                    
+                else:
+                    cluster_type = None
+                    architecture_type = None
+                    ensa_version = None
+                    filesystem_type = None
+                    hana_scenario = None
+
+
                 if hostname in self.result:
                     raise SupportFileException(f'{hostname} already present. Is "{file}" used twice?')
                 
